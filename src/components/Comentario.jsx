ac8fd4be079fa1bbaaf3943e656e1dbb8f6e2fa9
Cassio Luis Xavier da S.Fabiano <cassio.fabiano@trescon.com.br>
2022-03-19 20:09:43 -0300

    first commit

---
+++ src/components/Comentario.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Comentario.css'
+
+export default function Comentario() {
+    
+    return (
+        <div className="comentario">
+            <div>João:</div>
+            <div>Ola tudo bem?</div>
+        </div>
+    )
+}
