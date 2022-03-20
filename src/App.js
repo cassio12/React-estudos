372938b95fb18499135849a54cf7fbf22b313618
Cassio Luis Xavier da S.Fabiano <cassio.fabiano@trescon.com.br>
2022-03-19 23:56:28 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import Comentario from './components/Comentario';
 
+const comentar = [ {
+  nome: 'João',
+  email: 'joão.carlos@gmail.com',
+  data: new Date(2022, 3, 19),
+  mensagem: 'Olá, tuode bem?'
+},
+{
+  nome: 'Maria',
+  email: 'maria.madalena@gmail.com',
+  data: new Date(2022, 3, 19),
+  mensagem: 'Tudo, e com você?'
+},]
+
 function App() {
+  const [comentarios, setComentarios] = useState([])
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [mensagem, setMsn] = useState('')
+
+  const addComentario = (e) => {
+    e.preventDefault()
+    const novoComent = {
+      nome: nome,
+      email: email,
+      data: new Date(),
+      mensagem: mensagem
+    }
+
+    setComentarios([...comentarios, novoComent])
+    setNome('')
+    setEmail('')
+    setMsn('')
+  }
+
+  const remover = (comentario) => {
+    const novaLista = comentarios.filter(item => item !== comentario)
+    setComentarios(novaLista)
+  }
+
+  useEffect(() => {
+    setComentarios(comentar)
+  },[])
+
   return (
     <div className="App">
       <h1>Meu pimeiro projeto</h1>
-      <Comentario nome='João' email='João@gmail.com' data={new Date(2022, 3,19)}>
-        Olá tudo bem ?
-      </Comentario>
-      <Comentario nome='Maria' email='Maria@gmail.com' data={new Date(2022, 3, 23)}>
-        Tudo e com você?
-      </Comentario>
+
+      {comentarios.map((comentario, index) => (
+        <Comentario key={index}
+        nome={comentario.nome} 
+        email={comentario.email} 
+        data={comentario.data}
+        remove={() => remover(comentario)}
+        >
+          {comentario.mensagem}
+        </Comentario>
+      ))}
+      <form className='form' method='post' onSubmit={addComentario}>
+        <h2 className='title_form'>Adicinar Comentario</h2>
+        <div>
+          <input onChange={(e) => setNome(e.target.value)} name='nome' placeholder='Digite seu nome' value={nome}/>
+        </div>
+        <div>
+          <input onChange={(e) => setEmail(e.target.value)} type='email' name='email' placeholder='Digite seu email' value={email}/>
+        </div>
+        <div>
+          <textarea className='caixa_msg' onChange={(e) => setMsn(e.target.value)} name='mensagem' rows='4' value={mensagem}/>
+        </div>
+        <button className='btn_submit' type='subimit'>add comentario</button>
+      </form>
     </div>
   );
 }
--- src/components/Comentario.jsx
+++ src/components/Comentario.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { IoMdClose } from 'react-icons/io'
+import user from '../user.png'
+import { formatRelative } from 'date-fns'
 import './Comentario.css'
 
 export default function Comentario(props) {
+
+    const estilo = {
+        color: 'red',
+        paddin: '10px',
+        fontSize: '40px'
+    }
     
     return (
         <div className="comentario">
-            <h2>{props.nome}:</h2>
-            <p>{props.email}</p>
-            <p>{props.children}</p>
-            <p>{props.data.toString()}</p>
+            <img className="avatar" src={user} alt={props.nome}/>
+            <div className="conteudo">
+                <div>
+                    <h2 className="nome" >{props.nome}:</h2>
+                    <p className="email">{props.email}</p>
+                    <p className="mensagem">{props.children}</p>
+                </div>
+                <div>
+                    <IoMdClose className="remuve" onClick={props.remove}/>
+                    <p className="data">{formatRelative(props.data, new Date())}</p>
+                </div>
+            </div>
         </div>
     )
 }
