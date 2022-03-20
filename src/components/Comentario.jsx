5ae705cc77d97d07103fe40b2c6f0918c08be2c3
Cassio Luis Xavier da S.Fabiano <cassio.fabiano@trescon.com.br>
2022-03-20 13:28:03 -0300

    update

--- src/components/Comentario.jsx
+++ src/components/Comentario.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { IoMdClose } from 'react-icons/io'
 import user from '../user.png'
 import { formatRelative } from 'date-fns'
+import {ptBR} from 'date-fns/locale'
 import './Comentario.css'
 
 export default function Comentario(props) {
@@ -21,9 +22,9 @@ export default function Comentario(props) {
                     <p className="email">{props.email}</p>
                     <p className="mensagem">{props.children}</p>
                 </div>
-                <div>
+                <div className="box_end">
                     <IoMdClose className="remuve" onClick={props.remove}/>
-                    <p className="data">{formatRelative(props.data, new Date())}</p>
+                    <p className="data">{formatRelative(props.data, new Date(), {locale: ptBR})}</p>
                 </div>
             </div>
         </div>
